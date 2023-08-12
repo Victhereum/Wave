6b8ae42aa142555d97f2441e4c9876ec72ead144
Victhereum <tamsvictorisaac@gmail.com>
2023-08-12 18:26:10 +0100

    FIX: Setting up for deployment

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -o errexit  # exit on error
 
-pip install -r requirements.txt
+pip install -r requirements/production.txt
 
-python manage.py collectstatic
-make migrate
-make proxie-docs
+python manage.py collectstatic --noinput
+python manage.py spectacular --color --file schema.yml
+python manage.py migrate
