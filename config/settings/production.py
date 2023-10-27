44e4dc9be6053e4e861edfab5dd4c6b8c89cdd39
Victhereum <tamsvictorisaac@gmail.com>
2023-10-27 22:12:52 +0100

    Fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -82,13 +82,13 @@ AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
 AWS_S3_CUSTOM_DOMAIN = env(
     "DJANGO_AWS_S3_CUSTOM_DOMAIN",
-    default="https://s3.ap-southeast-1.wasabisys.com/wavesar",
+    default=f"{AWS_S3_REGION_NAME}.wasabisys.com/{AWS_STORAGE_BUCKET_NAME}",
 )
 AWS_S3_ENDPOINT_URL = env(
     "DJANGO_AWS_S3_ENDPOINT_URL",
-    default="https://s3.ap-southeast-1.wasabisys.com/wavesar",
+    default=f"{AWS_S3_REGION_NAME}.wasabisys.com/{AWS_STORAGE_BUCKET_NAME}",
 )
-AWS_S3_DOMAIN = aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"s3.ap-southeast-1.wasabisys.com/{AWS_STORAGE_BUCKET_NAME}"
+AWS_S3_DOMAIN = aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_S3_REGION_NAME}.wasabisys.com/{AWS_STORAGE_BUCKET_NAME}"
 # STATIC
 # ------------------------
 STORAGES = {
