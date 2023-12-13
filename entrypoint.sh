5e36209a1a404ecf7a23724e9d125f7803c32390
Victhereum <tamsvictorisaac@gmail.com>
2023-12-13 22:26:34 +0100

    Added delete endpoint to video

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ python manage.py collectstatic --noinput
 python manage.py compress
 python manage.py spectacular --color --file schema.yml
 python manage.py migrate
+python manage.py generatesuperuser
+
 exec /usr/local/bin/gunicorn config.asgi --bind 0.0.0.0:5000 --chdir=/app -k uvicorn.workers.UvicornWorker
